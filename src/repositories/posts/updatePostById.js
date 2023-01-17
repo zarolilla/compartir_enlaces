17fed8a1d1680c49de7cd6ce44b09724dafaa713
zarolilla <davidzarola@gmail.com>
2023-01-17 15:49:43 +0100

    solucionado update post

--- src/repositories/posts/updatePostById.js
+++ src/repositories/posts/updatePostById.js
@@ -6,7 +6,7 @@ const updatePostById = async (post) => {
   const pool = getPool();
 
   await pool.query(
-    'UPDATE posts SET title = ?, description = ? , link = ?,WHERE id = ?',
+    'UPDATE posts SET title = ?, description = ? , link = ? WHERE id = ?',
     [title, description, link, id]
   );
 };
